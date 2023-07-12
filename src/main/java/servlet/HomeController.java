39a53216f31369eed371741744055a0e9b909e18
LiS2Lim <s2alua.fu@gmail.com>
2023-07-12 10:19:03 +0900

    第11章 サーブレットクラスの実行の仕組みとフィルタ

--- src/main/java/servlet/HomeController.java
+++ src/main/java/servlet/HomeController.java
@@ -3,6 +3,7 @@ package servlet;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/")
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("home is inited");
+		config.getServletContext().setAttribute("text","text");
+	}
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		String path = "WEB-INF/jsp/home.jsp";
 		RequestDispatcher dispatcher = req.getRequestDispatcher(path);
 		dispatcher.forward(req,res);
+	}
+	
+	public void distroy() throws ServletException {
+		System.out.println("home is distroyed");
 	}
 }
